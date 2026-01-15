1d61b084e144c305392c9ce0a654c79724280a3f
BelajarCerdas <belajarcerdasdotid@gmail.com>
2026-01-16 02:56:39 +0700

    feat: add authentication middleware to protect restricted pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => redirect('/login'));
@@ -14,5 +15,8 @@ Route::get('/login', function () {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// DASHBOARD
-Route::get('/beranda', [DashboardController::class, 'index'])->name('beranda');
+// MIDDLEWARE LOGIN
+Route::middleware([AuthMiddleware::class])->group(function () {
+    // DASHBOARD
+    Route::get('/beranda', [DashboardController::class, 'index'])->name('beranda');
+});
