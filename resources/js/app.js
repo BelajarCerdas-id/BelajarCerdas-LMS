0861c17a8c15ffc1df9dab71d7c5d617e5d64750
BelajarCerdas <belajarcerdasdotid@gmail.com>
2026-01-17 18:14:50 +0700

    chore: install and configure pusher for realtime broadcasting

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,12 @@
 import './bootstrap';
 import Swal from 'sweetalert2';
-window.Swal = Swal; // supaya bisa dipakai global (di blade / jquery)
+window.Swal = Swal;
+
+import Echo from 'laravel-echo';
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true
+});
