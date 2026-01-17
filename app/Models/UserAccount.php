e6b1de5045c7ae496ccfee2ade7b15b03ba31b12
BelajarCerdas <belajarcerdasdotid@gmail.com>
2026-01-18 00:09:08 +0700

    feat(user-account): add relations to LMS, academic, and transaction models

--- app/Models/UserAccount.php
+++ app/Models/UserAccount.php
@@ -20,4 +20,44 @@ class UserAccount extends Authenticatable
     public function OfficeProfile() {
         return $this->hasOne(OfficeProfile::class, 'user_id');
     }
+
+    public function SchoolStaffProfile() {
+        return $this->hasOne(SchoolStaffProfile::class, 'user_id');
+    }
+
+    public function StudentProfile() {
+        return $this->hasOne(StudentProfile::class, 'user_id');
+    }
+
+    // SCHOOL PARTNER
+    public function SchoolPartner() {
+        return $this->hasOne(SchoolPartner::class, 'kepsek_id');
+    }
+
+    // TRANSACTION
+    public function Transaction() {
+        return $this->hasMany(Transaction::class, 'user_id');
+    }
+
+    // SYLLABUS
+    public function Kurikulum() {
+        return $this->hasMany(Kurikulum::class, 'user_id');
+    }
+
+    public function Fase() {
+        return $this->hasMany(Fase::class, 'user_id');
+    }
+
+    public function Kelas() {
+        return $this->hasMany(Kelas::class, 'user_id');
+    }
+
+    // LMS FEATURE
+    public function SchoolClass() {
+        return $this->hasOne(SchoolClass::class, 'wali_kelas_id');
+    }
+
+    public function StudentSchoolClass() {
+        return $this->hasMany(StudentSchoolClass::class, 'student_id');
+    }
 }
