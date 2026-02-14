9d894d088dd3379950f9d95a54b8e1f1cf611913
BelajarCerdas <belajarcerdasdotid@gmail.com>
2026-02-14 23:32:15 +0700

    feat: update app.js for support flatpickr

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
 import './bootstrap';
 import Swal from 'sweetalert2';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
 window.Swal = Swal;
 window.ClassicEditor = ClassicEditor;
+window.flatpickr = flatpickr;
 
 import Echo from 'laravel-echo';
 
