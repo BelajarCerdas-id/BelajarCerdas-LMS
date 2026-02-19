ad1b9c170403ab11cd595b19a6985d0a3165225c
BelajarCerdas <belajarcerdasdotid@gmail.com>
2026-02-19 17:04:21 +0700

    fix(pusher): remove duplicate Echo initialization in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,3 @@ import "flatpickr/dist/flatpickr.min.css";
 window.Swal = Swal;
 window.ClassicEditor = ClassicEditor;
 window.flatpickr = flatpickr;
-
-import Echo from 'laravel-echo';
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true
-});
