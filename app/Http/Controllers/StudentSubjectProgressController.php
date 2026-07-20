44c670d445f7919303598cda4d85d417619820c3
BelajarCerdas <belajarcerdasdotid@gmail.com>
2026-07-21 06:34:26 +0700

    fix(student-learning): load subject teacher based on student's school class

--- app/Http/Controllers/StudentSubjectProgressController.php
+++ app/Http/Controllers/StudentSubjectProgressController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Mapel;
+use App\Models\StudentSchoolClass;
+use Illuminate\Support\Facades\Auth;
 
 class StudentSubjectProgressController extends Controller
 {
@@ -13,10 +15,19 @@ class StudentSubjectProgressController extends Controller
 
     public function data($role, $schoolName, $schoolId, $curriculumId, $mapelId)
     {
-        $getMapel = Mapel::with(['TeacherMapel' => function ($q) {
-            $q->where('is_active', 1);
-            
-            },'TeacherMapel.UserAccount.SchoolStaffProfile'])->where('id', $mapelId)->first();
+        $studentSchoolClass = StudentSchoolClass::where('student_id', Auth::id())->where('student_class_status', 'active')->first();
+
+        if (!$studentSchoolClass) {
+            return response()->json([
+                'mapel' => null,
+            ]);
+        }
+
+        $schoolClassId = $studentSchoolClass->school_class_id;
+
+        $getMapel = Mapel::with(['TeacherMapel.UserAccount.SchoolStaffProfile', 'TeacherMapel' => function ($q) use ($schoolClassId) {
+            $q->where('is_active', 1)->where('school_class_id', $schoolClassId);
+        }])->where('id', $mapelId)->first();
 
         return response()->json([
             'mapel' => $getMapel,
